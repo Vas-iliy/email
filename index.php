fbbe51a3eb9e0869959749cde4011c718aa5d6b2
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-08 17:22:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<style>
+
+    input {
+        width: 300px;
+        margin: 5px;
+    }
+</style>
+<form action="" method="post">
+    <input type="text" name="username" required> <br>
+    <input type="email" name="email" required> <br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
