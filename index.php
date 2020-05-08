0efc5edf1f84aac33283a81a634a61e8e419bff4
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-08 20:03:46 +0300

    email

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?
+$connection = new PDO('mysql:host = localhost; dbname=email; charset=utf8',
+'root', 'root');
+
+function generateRandomString() {
+    $char = '0123456789qwertyuioplkjhgfdsazxcvbnmMNBVCXZASDFGHJKLPOIUYTREWQ';
+    $random = '';
+    for($i=0; $i<20; $i++) {
+        $random .= $char[rand(0, (strlen($char)-1))];
+
+    }
+    return $random;
+
+}
+
+if ($_POST['username']) {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $authKey = generateRandomString();
+    $query = $connection->query("INSERT INTO email (username, email, auth_key) 
+VALUES ('$username', '$email', '$authKey')");
+
+    if ($query) {
+        mail($email, 'Подтвердите почту', "Перейдите по ссылке 
+        http://email/?auth=$authKey");
+    }
+}
+
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,6 +40,15 @@
 </head>
 <body>
 <style>
+    * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Arial', sans-serif;
+    }
 
     input {
         width: 300px;
